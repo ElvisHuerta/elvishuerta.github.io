0742d4ce5c6d1cd91754778ef9fe91dc453b1162
ElvisH <elvis11uerta@gmail.com>
2020-10-09 02:35:55 -0500

    Declaración a enamorada

---
+++ js/script.js
@@ -0,0 +1,29 @@
+nombre = prompt("Escribe tu nombre");
+
+var btnno = document.getElementById('btnno');
+var btnyes = document.getElementById('btnyes');
+
+btnno.addEventListener('mouseenter', () => {
+    console.log("mouse entrando");
+    var pos = coordenadas();
+    btnno.style.top = `${pos.posy}px`;
+    btnno.style.left = `${pos.posx}px`;
+})
+
+var coordenadas = () => {
+    var ancho = screen.width - 250;
+    var alto = screen.height - 200;
+    var posx = Math.random() * (ancho - 0) + 0;
+    var posy = Math.random() * (alto - 0) + 0;
+    return { posx, posy }
+}
+
+if (!!nombre) {
+    btnyes.addEventListener('click', () => {
+        alert("Sabía que aceptarias ❤ " + nombre + " ❤");
+    })
+} else {
+    btnyes.addEventListener('click', () => {
+        alert("Sabía que aceptarias chiquilla, no podrias resistirte 💖💖");
+    })    
+}
